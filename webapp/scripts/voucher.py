25fc915d9f61dfd37208d4637b0a46d2a8b33d6c
PashaShulga <p-shulga.95@mail.ru>
2016-01-26 21:35:59 +0200

    secondary commit

--- webapp/scripts/voucher.py
+++ webapp/scripts/voucher.py
@@ -6,15 +6,15 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
 
-SMTP_USER = u'pavloshulga.95@gmail.com'
-SMTP_PASS = u'Reload_777'
+SMTP_USER = 'pavloshulga.95@gmail.com'
+SMTP_PASS = 'Reload_777'
 ENCODING = 'utf-8'
 
 
-def send_mail(email_to, subj, text, email_from='pavloshulga.95@gmail.com'):
+def send_mail(email_to, subj, text):
     _msg = MIMEMultipart()
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    _msg['from'] = Header(email_from, ENCODING)
+    _msg['from'] = Header(SMTP_USER, ENCODING)
     _msg['to'] = Header(email_to, ENCODING)
     _msg['subject'] = Header(subj, ENCODING)
     _msg_text = MIMEText(text.encode(ENCODING), 'plain', ENCODING)
@@ -26,7 +26,7 @@ def send_mail(email_to, subj, text, email_from='pavloshulga.95@gmail.com'):
     except smtplib.SMTPAuthenticationError as e:
         print(e)
     try:
-        server.sendmail(email_from, email_to, _msg.as_string())
+        server.sendmail(SMTP_USER, email_to, _msg.as_string())
     except smtplib.SMTPException as e:
         print(e)
     finally:
