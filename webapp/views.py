f6ed9a4658e16aaed5b39f1e1b9432fe934f15c3
PashaShulga <p-shulga.95@mail.ru>
2016-02-15 20:51:41 +0200

    fix bugs

--- webapp/views.py
+++ webapp/views.py
@@ -279,15 +279,17 @@ def pay_methods(request):
         credit_card = form.card.data
         sum_content = float(amount) * float(content) / 100
         sum_charity = float(amount) * float(charity) / 100
-        _bundle = DBSession.query(Bundle).filter(Bundle.date_start<=datetime.datetime.utcnow(),
-                                                 Bundle.date_end>=datetime.datetime.utcnow()).first()
+        # _bundle = DBSession.query(Bundle).filter(Bundle.date_start<=datetime.datetime.utcnow(),
+        #                                          Bundle.date_end>=datetime.datetime.utcnow()).first()
+        # _bundle = DBSession.query(Bundle).
+        print(request)
         codec = {
             'email': email,
             'card': credit_card,
             'charity': float(sum_charity),
             'content': float(sum_content),
             'amount': float(amount),
-            'bundle_id': _bundle.id
+            # 'bundle_id': _bundle.id
         }
         res = itsden_signat.dumps(codec)
         if float(form.amount.data) >= 2.0:
